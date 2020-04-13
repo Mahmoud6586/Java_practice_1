41683b0d1024f66412992436b76844a4bbe3418b
Mahmoud <mahmoudfekri@gmail.com>
2020-04-13 19:04:21 +0430

    Adding Selection sort method and finishing the main

--- src/Package1/SelectionSort.java
+++ src/Package1/SelectionSort.java
@@ -9,21 +9,39 @@ public class SelectionSort {
 
         System.out.println(" Enter the array length:\n");
         numSize = input.nextInt();
-        number = new int [numSize] ;
+        number = new int[numSize];
 
-        System.out.println(" Now enter" + numSize + " numbers:\n");
-        for (int i = 0; i < numSize ; i++){
-            number [i] = input.nextInt() ;
+        System.out.println(" Now enter " + numSize + " numbers:\n");
+        for (int i = 0; i < numSize; i++) {
+            number[i] = input.nextInt();
         }
+        SelectionSort example = new SelectionSort();
+        example.SelSort(number);
+        example.Print(number);
+    }
+
+    void SelSort(int numb[]) {
+        int maxIndex, temp, j;
+        //4 7  9 5
+
+        for (int i = 0; i < numb.length; i++) {
+            maxIndex = i;
+
+            for (j = i + 1; j < numb.length; j++) {
+                if (numb[maxIndex] < numb[j])
+                    maxIndex = j;
+            }
+            temp = numb[i];
+            numb[i] = numb[maxIndex];
+            numb[maxIndex] = temp;
+        }
+
+    }
+
+    void Print(int arr[]) {
+        for (int k = 0; k < arr.length; k++)
+            System.out.println(arr[k]);
+
     }
 }
 
-    void SeSort() {
-
-    }
-
-    void Print( int arr[]) {
-    for ( int k = 0 ; k < arr.length ; k++)
-        System.out.println(arr[k]);
-
-    }
