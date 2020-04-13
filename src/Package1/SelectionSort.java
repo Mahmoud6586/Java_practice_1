bbf0835e4787a5982354ca1cff26419b409d5033
Mahmoud <mahmoudfekri@gmail.com>
2020-04-13 17:46:27 +0430

    Creating selection sort project before methods

---
+++ src/Package1/SelectionSort.java
@@ -0,0 +1,27 @@
+package Package1;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numSize, number[];
+
+        System.out.println(" Enter the array length:\n");
+        numSize = input.nextInt();
+        number = new int [numSize] ;
+
+        System.out.println(" Now enter" + numSize + " numbers:\n");
+        for (int i = 0; i < numSize ; i++){
+            number [i] = input.nextInt() ;
+        }
+    }
+}
+
+    void SeSort() {
+
+    }
+
+    void Print(number[]) {
+
+    }
